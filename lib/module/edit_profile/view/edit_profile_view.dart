98b5e89257572b9e13f8ebed9f155e0f39167d53
unknown <rhidayscrew@gmail.com>
2024-06-04 13:44:45 +0700

    04_dahsboard_view_better_done

---
+++ lib/module/edit_profile/view/edit_profile_view.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:slicing_ui_one/core.dart';
+import '../controller/edit_profile_controller.dart';
+
+class EditProfileView extends StatefulWidget {
+  const EditProfileView({Key? key}) : super(key: key);
+
+  Widget build(context, EditProfileController controller) {
+  controller.view = this;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("EditProfile"),
+          actions: const [],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+            children: const [],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  State<EditProfileView> createState() => EditProfileController();
+}
+    
